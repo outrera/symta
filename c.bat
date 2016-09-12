d2059ff5d20b782c40566a41b8bffb3d29809021
Nikita Sadkov <snv985@gmail.com>
2016-09-12 21:38:04 +0300

    enabled optimization for symta.exe

--- c.bat
+++ c.bat
@@ -4,5 +4,6 @@ set wd=%~dp0
 set T___T=%PATH%
 set PATH=%wd%gcc\bin;%PATH%
 set cflags=-O1 -nostdlib -s -fno-exceptions -Wno-return-type -Wno-pointer-sign -D WINDOWS -I %1%
+rem gcc -Wl,--subsystem,windows
 gcc %cflags% -fpic -shared %2 -o %3
 set PATH=%T___T%
--- cr.bat
+++ cr.bat
@@ -1,6 +1,6 @@
 @echo off
 rem USE THIS SCRIPT TO RECOMPILE SYMTA.EXE
 set PATH=C:\Program Files\mingw-w64\x86_64-6.2.0-win32-seh-rt_v5-rev0\mingw64\bin;%PATH%
-set cflags=-O1 -Wno-return-type -Wno-pointer-sign -D WINDOWS -I ./runtime
+set cflags=-O2 -Wno-return-type -Wno-pointer-sign -D WINDOWS -I ./runtime
 set rtw=./runtime/w/
-gcc %cflags% -I %rtw% ./runtime/runtime.c %rtw%dlfcn.c %rtw%mman.c %rtw%sigaction.c %rtw%compat.c -o run.exe
+gcc %cflags% -I %rtw% ./runtime/runtime.c %rtw%dlfcn.c %rtw%mman.c %rtw%sigaction.c %rtw%compat.c -s -o symta.exe
