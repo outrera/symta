58d6d88ff683d854b9f6b35f70b0dc728d9a3a08
Nikita Sadkov <snv985@gmail.com>
2016-12-15 00:57:41 -0800

    update

--- cr.bat
+++ cr.bat
@@ -1,6 +1,6 @@
 @echo off
 rem USE THIS SCRIPT TO RECOMPILE SYMTA.EXE
-set PATH=C:\Program Files\mingw-w64\x86_64-6.2.0-win32-seh-rt_v5-rev0\mingw64\bin;%PATH%
+set PATH=C:\Users\nikita\Documents\tools\x86_64-6.2.0-release-win32-seh-rt_v5-rev1\mingw64\bin;%PATH%
 rem set PATH=C:\Program Files\mingw-w64\x86_64-6.2.0-posix-sjlj-rt_v5-rev1\mingw64\bin;%PATH%
 rem set PATH=C:\Program Files\mingw-w64\x86_64-4.8.2-posix-sjlj-rt_v3-rev4\mingw64\bin;%PATH%
 set cflags= -O2 -fno-stack-protector -fomit-frame-pointer -fno-exceptions -fno-unwind-tables -fno-ident -Wno-return-type -Wno-pointer-sign -D WINDOWS -I ./runtime
