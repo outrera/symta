855f838b03302f1d9e1fe88809e14b130fc4d36c
Nikita Sadkov <snv985@gmail.com>
2018-05-04 09:02:08 +0300

    slightly optimized GC

--- runtime/runtime_gc.h
+++ runtime/runtime_gc.h
@@ -2,14 +2,17 @@ GCDEF(gc_closure) {
   int i, size;
   frame_t *frame;
   void *p, *q;
+  void **pp, **oo;
   void *fixed_size, *dummy;
   api_t *api = &api_g;
   size = ((fn_meta_t*)get_meta(O_FN(o)))->size;
   GCPRE
   CLOSURE(p, O_CODE(o), size);
   MARK_MOVED(o,p);
+  pp = (void**)&REF(p,0);
+  oo = (void**)&REF(o,0);
   for (i = 0; i < size; i++) {
-    q = REF(o,i);
+    q = oo[i];
     frame = O_FRAME(q);
     if (frame == GCFrame) {
       q = gc_arglist(q);
@@ -19,7 +22,7 @@ GCDEF(gc_closure) {
         q = frame;
       }
     }
-    STOR(p, i, q);
+    pp[i] = q;
   }
   GCPOST
   return p;
@@ -28,13 +31,16 @@ GCDEF(gc_closure) {
 GCDEF(gc_list) {
   int i, size;
   void *p;
+  void **pp, **oo;
   api_t *api = &api_g;
   size = (int)UNFIXNUM(LIST_SIZE(o));
   GCPRE
   LIST_ALLOC(p, size);
   MARK_MOVED(o,p);
+  pp = (void**)&REF(p,0);
+  oo = (void**)&REF(o,0);
   for (i = 0; i < size; i++) {
-    GC_REC(REF(p,i), REF(o,i));
+    GC_REC(pp[i], oo[i]);
   }
   GCPOST
   return p;
@@ -92,14 +98,17 @@ GCDEF(gc_bytes) {
 GCDEF(gc_data) {
   int i, size;
   void *p;
+  void **pp, **oo;
   api_t *api = &api_g;
   uintptr_t tag = O_TAGH(o);
   size = types[tag].size;
   GCPRE
   ALLOC_DATA(p, tag, size);
   MARK_MOVED(o,p);
+  pp = (void**)&REF(p,0);
+  oo = (void**)&REF(o,0);
   for (i = 0; i < size; i++) {
-    GC_REC(REF(p,i), REF(o,i));
+    GC_REC(pp[i], oo[i]);
   }
   GCPOST
   return p;
